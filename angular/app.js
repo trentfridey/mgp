1853f450f0876b926ba408e66b99ad466a0ab876
System <none>
2017-10-03 17:36:34 +0000

    Split rule editor directive by general/specific

--- angular/app.js
+++ angular/app.js
@@ -12,22 +12,31 @@ app.controller('mgpRuleEditCtrl', function($scope){
   $scope.user.genRule.toAlive = {list: [1,2,3,4,5,6,7,8], current: 1}
 });
 
-app.directive('mgpRuleEditor', function(){
+app.directive('boxRuleEditor', function(){
 	return {
 		scope: {
-      type: '=',
-      options: '=',
-      selected: '='
+      type: '='
 		},
 		restrict: 'E',
-		require: '^ngModel',
+		//require: '^ngModel',
 		replace: true,
-		templateUrl: 'rule-editor.html',
-		link: function(scope, element, attrs, ngModel){
-			if(!ngModel) return;
-        scope.selectedOpt = function(opt){
-          scope.selected = opt;
-        }
+		templateUrl: 'box-rule-editor.html',
+		link: function(scope, element, attrs){
+      
+		}
+  }
+});
+
+app.directive('genRuleEditor', function(){
+	return {
+		scope: {
+      type: '='
+		},
+		restrict: 'E',
+		//require: '^ngModel',
+		replace: true,
+		templateUrl: 'gen-rule-editor.html',
+		link: function(scope, element, attrs){
       
 		}
   }
